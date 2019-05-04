5dcf5edc63894e2cf6b308e993c76198ff1b6836
fr4nsgallegos <38216891+fr4nsgallegos@users.noreply.github.com>
2019-05-04 16:40:09 -0500

    Update matrix.h

--- IV_matrix/matrix.h
+++ IV_matrix/matrix.h
@@ -1,30 +1,30 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#pragma once
+
 #include <iostream>
-
 using namespace std;
+template <class T>
+class matrix;
+template <class T>
 
-template<class T>
-ostream & operator << (ostream & os, const Matrix <T> & data);
+ostream& operator<<(ostream& os, const matrix<T> & dat);
 
-template<class T>
-class Matrix 
-{
-  private:
-    T **p_data;
-    size_t n_rows;
-	size_t n_cols;  
-  public:
-    Matrix(size_t nr=0,size_t nc=0);  
-	Matrix(T n_rows,T n_cols )
-    ~Matrix();  
-    T & operator ()(size_t i,size_t j);
-   	Matrix<T> operator + (const Matrix<T> & m1, Matrix<T> & m2);
-    Matrix<T> operator * (const Matrix<T> & m1,const Matrix<T> & m2);
-    Matrix<T> operator<<(T &v); 
-	friend ostream& operator << (ostream & os,const Matrix<T> & n); 
+template <class T>
+class matrix{
+	friend std::ostream &operator<< <T>(std::ostream &,const matrix<T> & );
+    private:
+        T** M;
+        size_t n_rows;
+        size_t n_columns;
+        int r = 0;
+        int c = 0;
+        
+    public:
+        matrix(size_t nr=0, size_t nc=0);
+        matrix(const matrix<T> & dat);
+        ~matrix();
+        T & operator()(size_t i, size_t j);
+        matrix<T> operator + (const matrix<T> & dat);
+        matrix<T> operator * (const matrix<T> & dat);
+        matrix<T>&operator << (const T v);
 };
-
-#endif
-
- 
+#include "matrix.inl"
