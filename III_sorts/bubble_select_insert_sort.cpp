7fcc379e1ac1876ef113628ba87152ad87420c2e
fr4nsgallegos <38216891+fr4nsgallegos@users.noreply.github.com>
2019-05-15 09:38:37 -0500

    Update bubble_select_insert_sort.cpp

--- III_sorts/bubble_select_insert_sort.cpp
+++ III_sorts/bubble_select_insert_sort.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 
-void ordenamientoburbuja(int *A,int n){
+void ordenamientoburbuja(int *A,int n)
+{
 	int temp;
 	int j;
-	 for (int i=1; i<n; i++){
-	 
-          for ( j=0 ; j<n - 1; j++){
-		  
-               if (A[j] > A[j+1]){
-			   
-                    temp = A[j];
-                    A[j] = A[j+1];
-                    A[j+1] = temp;}
-				}}
+	for (int i=1; i<n; i++)
+	{
+		for ( j=0 ; j<n - 1; j++)
+		{
+			if (A[j] > A[j+1])
+			{
+				temp = A[j];
+				A[j] = A[j+1];
+				A[j+1] = temp;
+			}
+		}
+	}
+}
 }
 
 void Ordenamiento_selec(int *a, int n)
@@ -34,39 +38,37 @@ void Ordenamiento_selec(int *a, int n)
 	}
 }
 
-void ordenamiento_inserc(int *A, int n){
+void ordenamiento_inserc(int *A, int n)
+{
 	int cont = 0;
-
-	for (int i = 1; i < n; i++) {
+	for (int i = 1; i < n; i++) 
+	{
 		int y = A[i];
 		int j = i - 1;
-
-		while ((j >= 0) && (A[j] > y)) {
+		while ((j >= 0) && (A[j] > y)) 
+		{
 			A[j + 1] = A[j];
 			j--;
 			cont++;
 		}
-
 		A[j + 1] = y;
 	}
-
-	
 }
 
 
-int main (){
+int main ()
+{
 	int n=7;
 	int array[]={5,4,2,7,8,9,7};
 	int *a = array;
-	 Ordenamiento_selec(array,n);
+	Ordenamiento_selec(array,n);
 	 //ordenamientoburbuja(array,n);
 	 //ordenamiento_inserc(array,n);
 
-
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n;i++)
+	{
 		cout<<array[i]<<" ";
 	}
 	return 0;
-
 }
 
